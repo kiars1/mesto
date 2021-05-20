4145d7449ec0e4963f17182222f0cb9bd96324b0
kiars1 <deadhaze317@yandex.ru>
2021-05-20 23:14:19 +0700

    HotFix review

--- scripts/index.js
+++ scripts/index.js
@@ -4,10 +4,9 @@ let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
 let popup = document.querySelector('.popup');
 let buttonClose = document.querySelector('.popup__button-close');
-let buttonSave = document.querySelector('.popup__button-save');
-let formElement = document.querySelector('.popup__container');
-let nameInput = document.querySelector('.popup__input-name');
-let jobInput = document.querySelector('.popup__input-info');
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('#nameInput');
+let jobInput = document.querySelector('#jobInput');
 
 function popupOpen() {
   popup.classList.add('popup_oppened');
@@ -25,7 +24,7 @@ function formSubmitHandler (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileDescription.textContent = jobInput.value;
+  popupClose()
 }
 formElement.addEventListener('submit', formSubmitHandler);
-buttonSave.addEventListener('click', popupClose);
 
