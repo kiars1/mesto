4c73c2d40fe6c1536217f6559841c58d216bda3e
kiars1 <deadhaze317@yandex.ru>
2021-06-18 23:13:06 +0700

    fix

--- scripts/validate.js
+++ scripts/validate.js
@@ -66,7 +66,7 @@ function toggleButtonState(inputList, buttonElement, rest) {
 }
 
 //Сбрасываем валидацию
-const refreshFormValidationState = (formElement, rest = validationConfig) => {
+const refreshinputValidity = (formElement, rest = validationConfig) => {
   const inputList = Array.from(formElement.querySelectorAll(rest.inputSelector));
   const buttonElement = formElement.querySelector(rest.submitButtonSelector);
 
